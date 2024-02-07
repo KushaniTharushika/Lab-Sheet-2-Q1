7166cd7ed29077ada5db49aa910ebabe432ebe2a
ASUS <ASUS@KUSHANI>
2024-02-07 13:58:50 +0530

    Add project files.

---
+++ Lab Sheet 2 Q1/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Sheet_2_Q1
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+   
+}
---
+++ Lab Sheet 2 Q1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Sheet_2_Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book mybook = new Book();
+            mybook.Title = "Village by the sea";
+            mybook.Author = "Anita Desai";
+
+            Console.WriteLine("Title: "+ mybook.Title);
+            Console.WriteLine("Author: "+ mybook.Author);
+
+            Console.ReadLine();
+        }
+    }
+}
